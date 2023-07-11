869d8afdbf21ddf004835bebde6fe702bfb11744
bipinemp <bhandaribipin3.39@gmail.com>
2023-07-11 21:56:19 +0545

    changes

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://socialmediafrontendapp.vercel.app",
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/users", userRoutes);
