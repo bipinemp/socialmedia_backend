a93e876e48c6e8f77d0e776a5f34ac3ce38db5cf
bipinemp <bhandaribipin3.39@gmail.com>
2023-07-11 21:52:38 +0545

    changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
+import cors from "cors";
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
@@ -18,6 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 // Routes
 app.use("/api/users", userRoutes);
