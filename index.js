bee49f1368883de455c3578217077735c3074f41
bipinemp <bhandaribipin3.39@gmail.com>
2023-07-11 18:49:05 +0545

    changes

--- index.js
+++ index.js
@@ -19,17 +19,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 
 // Routes
-app.use(
-  "/api/users",
-  cors({ origin: "http://localhost:3000", credentials: true }),
-  userRoutes
-);
+app.use("/api/users", cors({ origin: "*", credentials: true }), userRoutes);
 app.use(
   "/api/posts",
-  cors({ origin: "http://localhost:3000", credentials: true }),
+  cors({ origin: "*", credentials: true }),
   protect,
   postRoutes
 );
